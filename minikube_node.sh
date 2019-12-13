f2889ca1881cd9238a8a73099d56042f4c4a5edd
hng201 <hng201@caledonian.ac.uk>
2019-12-13 01:16:45 +0000

    Initial Commit

---
+++ minikube_node.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+sudo apt-get update
+
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+
+sudo apt install -y virtualbox
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+chmod +x minikube  
+
+sudo mv minikube /usr/local/bin/  
+
